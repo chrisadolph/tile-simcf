a8e505fe1eaf19b26ba57359eec419ee114eca4e
Aaron Erlich <aaron.erlich@gmail.com>
2012-07-25 17:13:14 -0700

    Updated test to be more comprehensive

--- testcfMake.r
+++ testcfMake.r
@@ -49,21 +49,27 @@ modelNA <- (y ~ log(M) + So + log(Ed) + log(Po1) + log(Po2)
               + log(U2) + log(GDP) + log(Ineq) + logitbound(Prob) +
               log(Time) + nas)
 
+model1 <- (y ~ log(M) + So)
+model2 <- (y ~ logitBound(Prob) + So)
+model3 <- (y ~ Prob + So)
+
 #test cfMake Warning messages
 tl = 1
 
-cfMake(model, tl)
-cfMake(formula = NULL, data = data) #should yield a warning
+notDFTest <- try(cfMake(model, tl)) # should yield a stop() that tl is not a data frame
+cfMake(formula = NULL, data = data[ ,(1:(ncol(data)-1))]) #should yield a warning but work 
 cfMake(modelNA, data) #should stop because no rows in data.frame
-
+cfMake(model1, data) #should work
+cfMake(model2, data) #should work with logitBound adjustment
 
 #Test the multiformula model
-model1 <- (y ~ log(M) + So)
-model2 <- (y ~ logitBound(Prob) + So)
-
-znb <- list(model1, model2)
-znbTest <- cfMake(formula = znb , data = data, nscen =5)
-print(znbTest)
+znb1 <- list(model1, model2) #with logitBound transformation
+znb2 <- list(model1, model3) #with no transformation
+znbError <- list(model1, model2, 5) #with an incorrectly specificed formula
+cfMake(formula = znb1 , data = data, nscen =5)
+cfMake(formula = znb2 , data = data, nscen =5)
+#should return an error saying one object of the list was not a formula but was of class numeric
+cfMake(formula = znbError , data = data, nscen =5)
 
 #use Gelman's arm smoking data for hierarchical example
 #data can be downloaded from
@@ -74,10 +80,23 @@ smokeL2 <- smoke[, c("newid", "wave", "smkreg")]
 smokeForm <- list((~ parsmk + sex.1.F.), (smkreg ~ wave))
 smokeData <- list(smokeL1, smokeL2)
 
-hlmTest <- cfMake(formula = smokeForm, data = smokeData, nscen =5)
+scenNam <- paste("scen", seq(1,5))
+scenNam2 <- paste("Hierarchical scen", seq(1,5))
+
+
+#Test normal functionality of  hierarchical cfChange and cfName
+hlmTest <- cfMake(formula = smokeForm, data = smokeData, nscen = c(8, 6))
+hlmTest <- cfChange(hlmTest, covname = "smkreg" , x =.8 , xpre = .3, scen = 2)
+hlmTest <- cfName(hlmTest, name = "some scen", scen =2,  df =1)
+hlmTest <- cfName(hlmTest, name = "some other scen", scen =3,  df =2)
 print(hlmTest)
 
-#test different lengths
+hlmTest2 <- hlmTest
+colnames(hlmTest2$x[[1]])[1] <- "smkreg"
+#should yield an error two covariates can't have the same name
+cfChange(hlmTest2, covname = "smkreg" , x =.8 , xpre = .3, scen = 2) 
+
+#test different lengths should yield an error
 cfMake(formula = smokeForm, data = list(smokeL1))
 
 
